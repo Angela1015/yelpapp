d875000ac2caaea3e927f3bafe584aa3a3967fcf
unknown <deberry1124@gmail.com>
2019-12-10 11:09:53 -0600

    fix broken link in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
+import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
